9e76307c24f0d04bd974990421769abb62c3a824
EzitoKo <ezequiel@losko.com.ar>
2019-04-12 19:56:49 -0300

    Commented gamelogic to make it somewhat readable

--- Python/gamelogic.py
+++ Python/gamelogic.py
@@ -1,23 +1,34 @@
+########################################
+#------AI/FRONTEND MUST MAKE SURE------#
+#----THE SQUARE ISN'T TAKEN ALREADY----#
+########################################
+
 import json
 import sys
 
 jsonfile = "../tablero.json"
 params = str(sys.argv[1]) + str(sys.argv[2])
 player = str(sys.argv[3])
-
+  
 def findsquare(boxtocheck):
-    print("boxtocheck " + boxtocheck)
-    with open(jsonfile, "r") as jayson:
-        data = json.load(jayson)
+    ## DUMPS JSON OBJECTS TO A PYTHON EDITABLE DICTIONARY
+    with open(jsonfile, "r") as jayson: 
+        data = json.load(jayson) 
+        ## LOOPS UNTILL IT FINDS THE DESIRED POSITION 
         for square in data["boxes"]:
             if square["cord"] == boxtocheck:
-                print(square["cord"])
+                ## MARKS POSITION AS TAKEN BY PLAYER
                 square["player"] = player
+                ## ADDS A POINT TO THE SQUARE
                 square["points"] += 1
+                ## CHECKS IF POINTS LIMIT IS REACHED
                 if square["points"] == square["max"]:
+                    ## RESET THE SQUARE'S POINTS
                     square["points"] = 0
+                    ## DUMPS THE MODIFIED DICTIONARY TO THE JSON FILE
                     with open(jsonfile, "w") as ndeah:
                         json.dump(data, ndeah, indent=4)
+                    ## TRIGGERS EXPANSION
                     print ("expanding")
                     if square["cordx"] == 2:
                         findsquare(str(square["cordx"] + 1) + str(square["cordy"]))
@@ -30,6 +41,7 @@ def findsquare(boxtocheck):
                     else: 
                         findsquare(str(square["cordx"]) + str(square["cordy"] + square["limy"]))
                 else:
+                    ## DUMPS THE MODIFIED DICTIONARY TO THE JSON FILE
                     with open (jsonfile, "w") as ndeah:
                         json.dump(data, ndeah, indent=4)
 
