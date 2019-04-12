88088f9256f30531fd457c0234b36ce65a17df36
EzitoKo <ezequiel@losko.com.ar>
2019-04-11 23:01:50 -0300

    FINALLY! Programmed the game's logic!

--- gamelogic.py
+++ gamelogic.py
@@ -9,29 +9,30 @@ def findsquare(boxtocheck):
     print("boxtocheck " + boxtocheck)
     with open(jsonfile, "r") as jayson:
         data = json.load(jayson)
-        for v in data["boxes"]:
-            print(v["cord"])
-            if v["cord"] == boxtocheck:
-                relsquare = v
-                addpoint(relsquare)
-                return
+        for square in data["boxes"]:
+            if square["cord"] == boxtocheck:
+                print(square["cord"])
+                square["player"] = player
+                square["points"] += 1
+                if square["points"] == square["max"]:
+                    square["points"] = 0
+                    with open(jsonfile, "w") as ndeah:
+                        json.dump(data, ndeah, indent=4)
+                    print ("expanding")
+                    if square["cordx"] == 2:
+                        findsquare(str(square["cordx"] + 1) + str(square["cordy"]))
+                        findsquare(str(square["cordx"] - 1) + str(square["cordy"]))
+                    else: 
+                        findsquare(str(square["cordx"] + square["limx"]) + str(square["cordy"]))
+                    if square["cordy"] == 2:
+                        findsquare(str(square["cordx"]) + str(square["cordy"] + 1))
+                        findsquare(str(square["cordx"]) + str(square["cordy"] - 1))
+                    else: 
+                        findsquare(str(square["cordx"]) + str(square["cordy"] + square["limy"]))
+                else:
+                    with open (jsonfile, "w") as ndeah:
+                        json.dump(data, ndeah, indent=4)
 
-def addpoint(square):
-    square["player"] = player
-    square["points"] += 1
-    if square["points"] == square["max"]:
-        square["points"] = 0
-        if square["cordx"] == 2:
-            findsquare(str(square["cordx"] + 1) + str(square["cordy"]))
-            findsquare(str(square["cordx"] - 1) + str(square["cordy"]))
-        else: 
-            findsquare(str(square["cordx"] + square["limx"]) + str(square["cordy"]))
-        if square["cordy"] == 2:
-            findsquare(str(square["cordx"]) + str(square["cordy"] + 1))
-            findsquare(str(square["cordx"]) + str(square["cordy"] - 1))
-        else: 
-            findsquare(str(square["cordx"]) + str(square["cordy"] + square["limy"]))
-#TODO: Rewrite JSON object
 
 
 if __name__ == "__main__":
