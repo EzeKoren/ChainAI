2cf95de3e6f0de8caebb9a19dec23999edcde272
EzitoKo <ezequiel@losko.com.ar>
2019-05-27 23:05:38 -0300

    i did crap

--- Python/AI.py
+++ Python/AI.py
@@ -13,7 +13,7 @@ def findtablero(num):
     while found == False:
         tablero = os.path.abspath(os.path.dirname(__file__)) + "/Tableros/" + str(num) + ".json"
         if os.path.isfile(tablero) == False:
-                print (tablero)
+                print(tablero)
                 found = True
                 open(tablero, "a+")
                 createfile(tablero)
@@ -21,6 +21,67 @@ def findtablero(num):
         return tablero
 
 tablero = findtablero(1)
+goal_steps = 10
+intial_games = 10000
+player = 1
+
+def model_data_preparation():
+    training_data = []
+    accepted_scores = []
+    for game_index in range(intial_games):
+        score = 0
+        game_memory = []
+        previous_observation = []
+        for step_index in range(goal_steps):
+            actionx = random.randrange(0, 6)
+            actiony = random.randrange(0, 10)
+            action = str(actionx) + str (actiony)
+            done, info = findsquare(action, tablero, player)
+            with open (tablero, "r") as jsonfile:
+                observation = json.load[jsonfile]
+            if len(previous_observation) > 0:
+                game_memory.append([previous_observation, action])
+            reward = 0
+            done = True
+            found1 = False
+            found2 = False
+            for square in observation["boxes"]:
+                if square[player] == player:
+                     found1 = True
+                     reward += 1
+                     if square["cordx"] or square["cordy"] == 0 or square["cordx"] == 5 or square["cordy"] == 9:
+                        reward += 1
+                        if square["cordx"] and square["cordy"] == 0 or square["cordx"] == 5 and square["cordy"] == 9:
+                                reward += 1
+
+
+                elif square[player] != 0:
+                     found2 = True
+                     reward -= 1
+                     if square["cordx"] or square["cordy"] == 0 or square["cordx"] == 5 or square["cordy"] == 9:
+                        reward -= 1
+                        if square["cordx"] and square["cordy"] == 0 or square["cordx"] == 5 and square["cordy"] == 9:
+                                reward -= 1
+            if found1 and found2 == True: done = False
+            previous_observation = observation
+            score += reward
+            if done:
+                break
+            
+        if score > 0:
+            accepted_scores.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0, 1]
+                elif data[1] == 0:
+                    output = [1, 0]
+                training_data.append([data[0], output])
+        
+        env.reset()
+
+    print(accepted_scores)
+    
+    return training_data
 
 def build_model(input_size, output_size):
     model = Sequential()
