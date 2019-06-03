b167a3cfc481d354a1820db3e61bfb51c4238fd0
EzitoKo <ezequiel@losko.com.ar>
2019-06-03 06:17:31 -0300

    de ruta

--- Android/app/src/main/java/com/ezito/chainaiclient/MainActivity.kt
+++ Android/app/src/main/java/com/ezito/chainaiclient/MainActivity.kt
@@ -2,11 +2,20 @@ package com.ezito.chainaiclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var curplayer = 1
+
+        btn00.setOnClickListener({
+            Toast.makeText(this, "DOU", Toast.LENGTH_LONG).show()
+        })
     }
+
 }
