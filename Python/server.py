7a92d9a348e69aaf81738a709717123a6023744d
EzitoKo <ezequiel@losko.com.ar>
2019-06-11 21:29:19 -0300

    Luca hizo andar esto xd

--- Python/server.py
+++ Python/server.py
@@ -1,22 +1,21 @@
-# from flask import Flask, request
-# from flask_cors import CORS
-# from gamelogic import findsquare as nut
+from flask import Flask, request
+from flask_cors import CORS
+from gamelogic import findsquare
+import json
 
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('../', methods=['POST'])
-# def manage_request():
-#     cord = request.form['cord']
-#     player = request.form['player']
-#     nut(cord, "Tableros/default.json", player)
-#     return 0
-
-# app.run()
-
-from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+CORS(app)
+
+@app.route('/input', methods=['POST'])
+
+def manage_request():
+    cord = request.form['cord']
+    player = request.form['player']
+    print("player: {0}, coord: {1}".format(player, cord))
+    findsquare(cord, "../Tableros/default.json", player)
+    #with open("Tableros/default.json", "r") as json_file:
+    #    return json.dumps(json_file)
+    return json.dumps("{'text':'hola'}")
+
+app.run()
--- Ui/script.js
+++ Ui/script.js
@@ -4,15 +4,24 @@ $(window).on("load", function() {
         console.log("ready!");
         M.AutoInit();
         $('#c00').click(function() {
-            M.toast({ html: 'Hello, world' })
-            $.ajax({
-                type: "POST",
-                url: "../Python/testfile.py",
-                data: { param: "text" }
-            }).done(function(o) {
-                M.toast({ html: 'done' })
-            });
-            // MAS CODIGO ACA
+            handle_input(34, "hola")
         });
     });
 });
+
+var url = "http://127.0.0.1:5000/input";
+
+function handle_input(cord, player) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    var data = new FormData();
+    data.append("cord", cord)
+    data.append("player", player)
+    xhr.send(data);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(xhr.responseText);
+        }
+    };
+}
