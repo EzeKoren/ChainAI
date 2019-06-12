f4004047fc6a63a3fc5bda4015a599032b76871f
EzitoKo <ezequiel@losko.com.ar>
2019-06-12 15:53:09 -0300

    started with anim

--- Python/gamelogic.py
+++ Python/gamelogic.py
@@ -12,23 +12,32 @@ import time
 # params = str(sys.argv[1]) + str(sys.argv[2])
 # player = str(sys.argv[3])
   
-def findsquare(boxtocheck, jsonfile, player):
+def findsquare(boxtocheck, jsonfile, player, prevanim, animnum):
+    anim = {}
+    animnum += 1
     ## DUMPS JSON OBJECTS TO A PYTHON EDITABLE DICTIONARY
     with open(jsonfile, "r") as jayson: 
         data = json.load(jayson) 
         ## LOOPS UNTILL IT FINDS THE DESIRED POSITION 
         for square in data["boxes"]:
             if square["cord"] == boxtocheck:
+                animnum += 1
+                anim["cord"] = "\#" + boxtocheck
                 ## MARKS POSITION AS TAKEN BY PLAYER
-                time.sleep(.5)
+                ## time.sleep(.5)
                 square["player"] = player
+                anim["player"] = player
                 ## ADDS A POINT TO THE SQUARE
                 square["points"] += 1
+                anim["points"] = square["points"]
                 ## CHECKS IF POINTS LIMIT IS REACHED
                 if square["points"] == square["max"]:
                     ## RESET THE SQUARE'S POINTS
                     square["points"] = 0
                     square["player"] = 0
+                    anim["points"] = 0
+                    anim["player"] = 0
+                    prevanim[animnum] = anim
                     ## DUMPS THE MODIFIED DICTIONARY TO THE JSON FILE
                     with open(jsonfile, "w") as ndeah:
                         json.dump(data, ndeah, indent=4)
@@ -48,3 +57,5 @@ def findsquare(boxtocheck, jsonfile, player):
                     ## DUMPS THE MODIFIED DICTIONARY TO THE JSON FILE
                     with open (jsonfile, "w") as ndeah:
                         json.dump(data, ndeah, indent=4)
+                    prevanim[animnum] = anim
+
